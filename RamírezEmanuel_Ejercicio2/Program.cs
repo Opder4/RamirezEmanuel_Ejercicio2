e0e95b3174f6b16a62681d5e95dac1caabe1df40
Emanuel Ramírez <er6348100@gmail.com>
2026-02-21 17:35:07 -0600

    Menú y los datos de entrada

--- RamírezEmanuel_Ejercicio2/Program.cs
+++ RamírezEmanuel_Ejercicio2/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿Console.WriteLine("Seleccione su Rol:");
+Console.WriteLine("");
+Console.WriteLine("1. Estudiante");
+Console.WriteLine("2. Docente");
+Console.WriteLine("3. Técnico IT");
+Console.WriteLine("4. Visistante");
+Console.WriteLine("");
+int rol=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese la hora de ingreso (0-23): ");
+Console.WriteLine("");
+int hora=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingres el número de día (1-7): ");
+Console.WriteLine("");
+int dia=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Tiene un carnet vigente? (s/n)");
+Console.WriteLine("");
+string carne=Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("Tiene autorización escrita? (s/n)");
+Console.WriteLine("");
+string autorizacion = Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("Trae una USB? (s/n)");
+string usb=Console.ReadLine();
+if (usb==s)
+{
+    Console.WriteLine("Está acompañado? (s/n)");
+    string compania=Console.ReadLine();
+}
